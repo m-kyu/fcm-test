cf7e3e3ad26158c11d36cb921dd888e152e40931
m-kyu <yicha7@gmail.com>
2024-03-19 09:35:18 +0900

    sdf

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,22 @@
+
 import Footer from "@/comp/Footer";
 import Header from "@/comp/Header";
+import axios from "axios";
 
 function about() {
+    async function getData(){
+        const a = await axios.get('/api/100000');
+        console.log(a.data)
+
+    }
+
+
     return (
         <>
             <Header/>
             <div style={{height:'2000px'}}>
                 어바웃 내용.....
+                <button onClick={getData}>가져오기</button>
             </div>
             <Footer/>
         </>
